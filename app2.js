062eb67eb04199fbe748e5f3ae1d0853ffe46cfc
lemmo <ltensok@gmail.com>
2018-11-24 04:09:22 +0200

    png to canvas

--- app2.js
+++ app2.js
@@ -25,24 +25,6 @@ let arr = [
   ]
 ];
 
-// let html1 = '<div>I ASTE (leib ja muud teraviljatooted; kartul; puder, makaronid e pasta, riis, helbed).' + 
-// '</div> <div>Kui mitu portsjonit päevas sööd kalatooteid</div> <div class="form-check"> <input class="form-check-input"' +
-// 'type="radio" id="exampleRadios1" value="option1"> <label class="form-check-label" for="exampleRadios1"> ühe portsoni ' +
-// '</label> </div> <div> <button type="button" class="btn btn-primary" onclick="genhtml()">Edasi</button> </div>'
-
-// let html2 = '<div>I ASTE (leib ja muud teraviljatooted; kartul; puder, makaronid e pasta, riis, helbed).' + 
-// '</div> <div>Kui mitu portsjonit päevas sööd kalatooteid</div> <div class="form-check"> <input class="form-check-input"' +
-// 'type="radio" id="exampleRadios1" value="option1"> <label class="form-check-label" for="exampleRadios1"> ühe portsoni ' +
-// '</label> </div> <div> <button type="button" class="btn btn-primary" onclick="vajuta2()">Edasi</button> </div>'
-
-// let html = function() {
-//   for (let i = 0; i < arr.length; i++) {
-//     let level = `<div>${arr[i][0]}</div>`;
-//     for (let j = 0; j < arr[i].length; j++) {
-      
-//     }
-//   }
-// };
 
 let genhtml = function() {
   html = '';
@@ -57,24 +39,153 @@ let genhtml = function() {
   page ++;
 };
 
-// '<div>${arr[page][0]}</div>'
-// '<div>${arr[page][1]}</div>'
-// '<div>'
-//   '<ul>'
-//     '<li>${arr[page][i]}</li>'
-//   '</ul>'
-// '</div>'
-// '<button type="button" class="btn btn-primary" onclick="vajuta2()">Edasi</button></div>'
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
+let red = new Image();
+let gre = new Image();
+let blu = new Image();
+let ora = new Image();
+let yel = new Image();
+let gr2 = new Image();
+let bl2 = new Image();
+let re2 = new Image();
+let mag = new Image();
+let gry = new Image();
+let re3 = new Image();
+let sred = new Image();
+let sblu = new Image();
+let smag = new Image();
 
-// function vajuta() {
-//   target = document.getElementById('textbox');
-//   target.innerHTML = html1;
-// }
+red.src = "img/red.png"
+gre.src = "img/gre.png"
+blu.src = "img/blu.png"
+ora.src = "img/ora.png"
+yel.src = "img/yel.png"
+gr2.src = "img/gr2.png"
+bl2.src = "img/bl2.png"
+re2.src = "img/re2.png"
+mag.src = "img/mag.png"
+gry.src = "img/gry.png"
+re3.src = "img/re3.png"
+sred.src = "img/sred.png"
+sblu.src = "img/sblu.png"
+smag.src = "img/smag.png"
 
-// function vajuta2() {
-//   target = document.getElementById('textbox');
-//   target.innerHTML = html2;
-//   console.log('vajuta2');
-// }
+function drawInitalPyramid() {
+  ctx.beginPath();
+  ctx.moveTo(0,200);
+  ctx.lineTo(200,200);
+  ctx.lineTo(100,26);
+  ctx.fillStyle = "grey";
+  ctx.fill();
+}
 
-//vajuta();
+function placeImage(img, x, y) {
+  ctx.drawImage(tile, 30, 30);  
+}
+
+//placeImage(tile, 10, 10);
+
+// tile.addEventListener('load', function() {
+//   ctx.drawImage(tile, 10, 10);
+// }, false);
+
+function drawLevel() {
+
+  // TASE I
+  // Leib
+  ctx.drawImage(blu, 0, 900);
+  ctx.drawImage(blu, 60, 900);
+  ctx.drawImage(blu, 120, 900);
+  ctx.drawImage(blu, 180, 900);
+
+  // Kartul
+  ctx.drawImage(gre, 240, 900);
+  ctx.drawImage(gre, 300, 900);
+  
+  // Teravili
+  ctx.drawImage(yel, 360, 900);
+  ctx.drawImage(yel, 420, 900);
+
+  // TASE 2
+  // Juurviljad
+  ctx.drawImage(red, 30, 790);
+  ctx.drawImage(red, 90, 790);
+  ctx.drawImage(red, 150, 790);
+  ctx.drawImage(red, 210, 790);
+  ctx.drawImage(red, 270, 790);
+
+  // Puuviljad
+  ctx.drawImage(ora, 330, 790);
+  ctx.drawImage(ora, 390, 790);
+  
+  // TASE 3
+  // Liha
+  ctx.drawImage(gr2, 30, 680);
+  ctx.drawImage(gr2, 90, 680);
+
+  // Kala
+  ctx.drawImage(bl2, 150, 680);
+  ctx.drawImage(bl2, 210, 680);
+
+  // Piim
+  ctx.drawImage(re2, 270, 680);
+  ctx.drawImage(re2, 330, 680);
+
+  // Muna
+  ctx.drawImage(mag, 390, 680);
+
+  // TASE 4
+  // Toidurasvad, pähklid
+  ctx.drawImage(gry, 150, 570);
+  ctx.drawImage(gry, 210, 570);
+  ctx.drawImage(gry, 270, 570);
+
+  // TASE 5
+  // SNÄKID
+  ctx.drawImage(re3, 180, 460);
+  ctx.drawImage(re3, 240, 460);
+
+}
+
+function drawLevel2 () {
+  ctx.drawImage(sblu, 0, 900);
+  ctx.drawImage(sblu, 7, 900);
+  ctx.drawImage(sblu, 14, 900);
+  ctx.drawImage(sblu, 21, 900);
+  ctx.drawImage(sblu, 28, 900);
+  ctx.drawImage(sblu, 35, 900);
+  ctx.drawImage(sblu, 42, 900);
+  ctx.drawImage(sblu, 49, 900);
+  ctx.drawImage(sblu, 56, 900);
+  
+  ctx.drawImage(sred, 63, 900);
+  ctx.drawImage(sred, 70, 900);
+  ctx.drawImage(sred, 77, 900);
+  ctx.drawImage(sred, 84, 900);
+  ctx.drawImage(sred, 91, 900);
+  ctx.drawImage(sred, 98, 900);
+  ctx.drawImage(sred, 105, 900);
+  ctx.drawImage(sred, 112, 900);
+  ctx.drawImage(sred, 112, 900);
+
+  ctx.drawImage(smag, 25, 798);
+  ctx.drawImage(smag, 32, 798);
+  ctx.drawImage(smag, 39, 798);
+  ctx.drawImage(smag, 46, 798);
+  ctx.drawImage(smag, 53, 798);
+  ctx.drawImage(smag, 60, 798);
+  ctx.drawImage(smag, 67, 798);
+
+
+
+
+
+
+
+}
+
+
+
+
+
